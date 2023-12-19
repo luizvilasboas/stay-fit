4642f347dc1c0b0d6d6352b1fb76332d8d6ec0a2
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2023-12-19 15:39:22 -0300

    Adding base project

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { Box } from '@mui/material'
+import HeroBanner from '../components/HeroBanner'
+import SearchExercises from '../components/SearchExercises'
+import Exercises from '../components/Exercises'
+
+const Home = () => {
+  const [exercises, setExercises] = useState([])
+  const [bodyPart, setBodyPart] = useState('all')
+
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises setExercises={setExercises} setBodyPart={setBodyPart} bodyPart={bodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+    </Box>
+  )
+}
+
+export default Home
